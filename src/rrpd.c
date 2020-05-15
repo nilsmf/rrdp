59f38071d68f1f9876027586879303adad5ab428
Nils Fisher <nils_fisher@hotmail.com>
2020-05-15 10:09:36 +1000

    use libxml. but no need to switch to libexpat.

--- src/rrpd.c
+++ src/rrpd.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <err.h>
+
 #include <unistd.h>
+
+#include <err.h>
+
 #include <curl/curl.h>
+#include <libxml/xmlreader.h>
+#include <libxml/parser.h>
 
 // libxm (l?)
 // ftp/curl
@@ -37,6 +42,7 @@ void cleanopts(opts *o) {
 }
 
 void fetch_snapshots(FILE* snapshot_file_input) {
+	//CURL feetch
 /*	CURL *curl = curl_easy_init();
 	int res = -1;
 	if(curl) {
@@ -51,38 +57,85 @@ void fetch_snapshots(FILE* snapshot_file_input) {
 	fprintf(snapshot_file_input, "bye world %d\n", res);
 */
 	//printf("writing to pipe\n");
+	
+
+	//STATIC
+	/*
 	char str[] = "hello world 1234 1234 1234\n";
 	for (int i=0; i<5; i++) {
 		//write(snapshot_file_input, str, strlen(str) + 1);
 		fprintf(snapshot_file_input, "%d - %s", i, str);
 		fflush(snapshot_file_input);
-	}
+	}*/
 
-}
-
-void process_snapshots(FILE* snapshot_file_output) {
-	char read_buffer[200];
-	//printf("reading\n");
-	while (fgets(read_buffer, 200, snapshot_file_output)) {
-		//printf("%ld chars read:\n", strlen(read_buffer));
-		printf("%.200s", read_buffer);
-	}
-		
-/*	char *buffer_ptr;
-	int chars_read;
-	int chunk_size;
-	while ((chars_read = read(snapshot_file_output, read_buffer, sizeof(read_buffer))) != 0) {
-		printf("read\n");
-		buffer_ptr = read_buffer;
-		//TODO handle extra bits in case read_buffer couldnt fit whole message
-		for(chunk_size = strlen(buffer_ptr) + 1; chunk_size > 1 && chars_read >= chunk_size;) {
-			printf("%.200s", buffer_ptr);
-			chars_read -= chunk_size;
-			buffer_ptr += chunk_size;
-			chunk_size = strlen(buffer_ptr) + 1;
+	FILE *snapshot_file_disk = fopen("regress/snapshot.xml", "r");
+	if (snapshot_file_disk) {
+		char read_buffer[200];
+		//printf("reading\n");
+		while (fgets(read_buffer, 200, snapshot_file_disk)) {
+			//printf("%ld chars read:\n", strlen(read_buffer));
+			fprintf(snapshot_file_input, "%.200s", read_buffer);
+			fflush(snapshot_file_input);
 		}
 	}
-*/
+}
+
+static void print_element_names(xmlNode * a_node)
+{
+	xmlNode *cur_node = NULL;
+
+	for (cur_node = a_node; cur_node; cur_node = cur_node->next) {
+		if (cur_node->type == XML_ELEMENT_NODE) {
+			printf("node type: Element, name: %s\n", cur_node->name);
+		}
+
+		print_element_names(cur_node->children);
+	}
+}
+
+void publish_node(xmlDoc *doc, xmlNode *publish_node) {
+	xmlElemDump(stdout, doc, publish_node);
+}
+
+void process_snapshots(int snapshot_fd) {
+	xmlValidCtxtPtr ctxt = NULL;
+	xmlDoc *doc = NULL;
+	xmlNode *root_element = NULL;
+
+	ctxt = xmlNewParserCtxt();
+	if (!ctxt) {
+		fprintf(stderr, "xml ctxt allocation failed");
+		return;
+	}
+
+	ctxt->userData = stdout; 
+	ctxt->error    = (xmlValidityErrorFunc) fprintf;   /* register error function */ 
+	ctxt->warning  = (xmlValidityWarningFunc) fprintf; /* register warning function */ 
+
+	doc = xmlCtxtReadFd(ctxt, snapshot_fd, "", NULL, 0);
+	if (!doc) {
+		fprintf(stderr, "parse failed");
+		xmlFreeParserCtxt(ctxt);
+		return;
+	}
+	if (ctxt->valid != 0) {
+		fprintf(stderr, "not valid");
+		xmlFreeDoc(doc);
+		xmlFreeParserCtxt(ctxt);
+		return;
+	}
+
+
+	root_element = xmlDocGetRootElement(doc);
+	for (xmlNode *cur_node = root_element; cur_node; cur_node = cur_node->next) {
+		printf("reading node");
+		if (cur_node->type == XML_ELEMENT_NODE && !strcmp(cur_node->name, "publish")) {
+			publish_node(doc, cur_node);
+		}
+	}
+
+	xmlFreeDoc(doc);
+	xmlFreeParserCtxt(ctxt);
 }
 
 int main(int argc, char** argv) {
@@ -90,7 +143,6 @@ int main(int argc, char** argv) {
 	opts *options;
 	int snapshot_file_pipe[2];
 	FILE *snapshot_file_in;
-	FILE *snapshot_file_out;
 	
 	options = getopts(argc, argv);
 	if( pipe(snapshot_file_pipe) != 0)
@@ -112,8 +164,7 @@ int main(int argc, char** argv) {
 		err(1, "fork");
 	/* snapshot processor */
 	if (pid == 0) {
-		snapshot_file_out = fdopen(snapshot_file_pipe[0], "r");
-		process_snapshots(snapshot_file_out);
+		process_snapshots(snapshot_file_pipe[0]);
 	}
 
 	cleanopts(options);
