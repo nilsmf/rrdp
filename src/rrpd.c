79ef59f63eecd0f587803a5a9eb83b3deef7565e
Nils Fisher <nils_fisher@hotmail.com>
2020-05-25 16:02:30 +1000

    enable the curl style again... currently it fails to produce the same result as the from file version... damn

--- src/rrpd.c
+++ src/rrpd.c
@@ -3,7 +3,6 @@
 #include <string.h>
 
 #include <unistd.h>
-
 #include <err.h>
 
 #include <curl/curl.h>
@@ -15,7 +14,7 @@
 // 
 // xml has urls to download the files
 //
-// rrpd servers 
+// rrdp servers
 // step 0. fetch notification files
 // 	step 0.1 process notification files
 // step 1. fetch snapshot
@@ -50,19 +49,24 @@ void cleanopts(opts *o) {
 
 void fetch_snapshots(FILE* snapshot_file_input) {
 	//CURL fetch
-/*	CURL *curl = curl_easy_init();
+	CURL *curl = curl_easy_init();
 	int res = -1;
 	if(curl) {
-		fprintf(snapshot_file_input, "hello world\n");
+		printf("starting curl\n");
+		fflush(stdout);
 		CURLcode res;
-		curl_easy_setopt(curl, CURLOPT_URL, "http://www.shouldistartdrinking.com/");
+		curl_easy_setopt(curl, CURLOPT_URL, "https://ca.rg.net/rrdp/eaf7cb9c-717a-4c02-b683-4ee3820ab3d0/snapshot/5740.xml");
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, snapshot_file_input);
 		res = curl_easy_perform(curl);
-		fprintf(snapshot_file_input, "curl response: %d\n", res);
+		printf("curl response: %d\n", res);
+		fflush(stdout);
 		curl_easy_cleanup(curl);
+	} else {
+		err(1, "curl init failure");
 	}
-	fprintf(snapshot_file_input, "bye world %d\n", res);
-*/
+	printf("bye world %d\n", res);
+	fflush(stdout);
+
 	//printf("writing to pipe\n");
 	
 
@@ -74,8 +78,10 @@ void fetch_snapshots(FILE* snapshot_file_input) {
 		fprintf(snapshot_file_input, "%d - %s", i, str);
 		fflush(snapshot_file_input);
 	}*/
+}
 
-	FILE *snapshot_file_disk = fopen("regress/snapshot.xml", "r");
+void fetch_snapshots_static(FILE* snapshot_file_input) {
+	FILE *snapshot_file_disk = fopen("regress/5740.xml", "r");
 	if (snapshot_file_disk) {
 		char read_buffer[200];
 		//printf("reading\n");
@@ -143,6 +149,7 @@ void snapshot_elem_start(void *data, const char *el, const char **attr) {
 
 		snapshot_xml->scope = SNAPSHOT_SCOPE_SNAPSHOT;
 		printf("start %s\n", el);
+		fflush(stdout);
 		print_snapshot_xml(snapshot_xml);
 	// Will enter here multiple times, BUT never nested. will start collecting character data in that handler
 	// mem is cleared in end block, (TODO or on parse failure)
@@ -153,6 +160,7 @@ void snapshot_elem_start(void *data, const char *el, const char **attr) {
 		for (int i = 0; attr[i]; i += 2) {
 			if (strcmp("uri", attr[i]) == 0) {
 				snapshot_xml->publish_uri = strdup(attr[i+1]);
+			} else if (strcmp("xmlns", attr[i]) == 0) {
 			} else {
 				err(1, "parse failed - non conforming attribute found in publish elem");
 			}
@@ -185,7 +193,7 @@ void snapshot_elem_end(void *data, const char *el) {
 			err(1, "parse failed - no data recovered from publish elem");
 		}
 		//TODO write this data somewhere (and/or never keep this much and stream it straight to staging file?)
-		printf("publish: '%.*s'\n", snapshot_xml->publish_data ? snapshot_xml->publish_data_length : 4, snapshot_xml->publish_data ?: "NULL");
+		//printf("publish: '%.*s'\n", snapshot_xml->publish_data ? snapshot_xml->publish_data_length : 4, snapshot_xml->publish_data ?: "NULL");
 		free(snapshot_xml->publish_uri);
 		snapshot_xml->publish_uri = NULL;
 		free(snapshot_xml->publish_data);
@@ -207,7 +215,7 @@ void snapshot_content_handler(void *data, const char *content, int length)
 		if (length == 1 && content[0] == '\n') {
 			return;
 		}
-		printf("parse chunk %d\n", length);
+		//printf("parse chunk %d\n", length);
 		//append content to publish_data
 		if (snapshot_xml->publish_data) {
 			snapshot_xml->publish_data = realloc(snapshot_xml->publish_data, sizeof(char)*(snapshot_xml->publish_data_length + length));
@@ -223,7 +231,7 @@ void snapshot_content_handler(void *data, const char *content, int length)
 		//printf("publish_data running total (%d) '%.*s'\n", snapshot_xml->publish_data_length, snapshot_xml->publish_data_length, snapshot_xml->publish_data);
 	}
 	else {
-		printf("chars found '%.*s'\n", length, content);
+		//printf("chars found '%.*s'\n", length, content);
 	}
 }
 
@@ -232,13 +240,15 @@ void process_snapshots(FILE* snapshot_file_out) {
 	char read_buffer[BUFF_SIZE];
 	SNAPSHOT_XML *snapshot_xml = calloc(1, sizeof(SNAPSHOT_XML));
 	XML_Parser p = XML_ParserCreate(NULL);
+
 	XML_SetElementHandler(p, snapshot_elem_start, snapshot_elem_end);
 	XML_SetCharacterDataHandler(p, snapshot_content_handler);
 	XML_SetUserData(p, (void*)snapshot_xml);
 	//printf("reading\n");
 	while (fgets(read_buffer, BUFF_SIZE, snapshot_file_out)) {
 		//printf("%ld chars read:\n", strlen(read_buffer));
-		//printf("%.200s", read_buffer);
+		//printf("%.200s\n", read_buffer);
+		fflush(stdout);
 		if (!XML_Parse(p, read_buffer, strlen(read_buffer), 0)) {
 			fprintf(stderr, "Parse error at line %lu:\n%s\n",
 				XML_GetCurrentLineNumber(p),
@@ -267,19 +277,20 @@ int main(int argc, char** argv) {
 		close(snapshot_file_pipe[0]);
 		snapshot_file_in = fdopen(snapshot_file_pipe[1], "w");
 		fetch_snapshots(snapshot_file_in);
+		//fetch_snapshots_static(snapshot_file_in);
 		close(snapshot_file_pipe[1]);
 		exit(0);
 	}
 	close(snapshot_file_pipe[1]);
 
-	if ((pid = fork()) == -1)
-		err(1, "fork");
+	//if ((pid = fork()) == -1)
+	//	err(1, "fork");
 	/* snapshot processor */
-	if (pid == 0) {
+	//if (pid == 0) {
 		snapshot_file_out = fdopen(snapshot_file_pipe[0], "r");
 		process_snapshots(snapshot_file_out);
 		close(snapshot_file_pipe[0]);
-	}
+	//}
 
 	cleanopts(options);
 }
