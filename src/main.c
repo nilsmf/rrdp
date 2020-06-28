8367a0c86180f3a4c140b5d8820043496b6839f8
Claudio Jeker <claudio@openbsd.org>
2020-06-28 12:04:35 +0200

    Add some of my todos

--- src/main.c
+++ src/main.c
@@ -38,6 +38,11 @@
  * - exit early from xml parsing if we know we are ok already?
  * - start to handle errors better
  * - nice to have optimise with keep alives etc.
+
+ * - check for error of malloc/calloc and strdup functions
+ * - replace printf with more elaborate reporting (log_warn / log_debug...)
+ * - kill newOpts, cleanupopts use stack variable instead
+ * - use openat, renameat, linkat, unlinkat etc instead of full pathes
  */
 
 static void
