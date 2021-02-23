c0ec6723aa0ea53c4a74d87d5ccb8c4004bc37c1
Claudio Jeker <claudio@openbsd.org>
2021-02-23 17:04:30 +0100

    Add some more scaffolding for later. Does not do anything yet.

--- rpki-client/rrdp.c
+++ rpki-client/rrdp.c
@@ -23,12 +23,20 @@
 #include <unistd.h>
 #include <imsg.h>
 
+#include <expat.h>
+
 #include "extern.h"
 #include "rrdp.h"
 
+#define	READ_BUF_SIZE	(32 * 1024)
 #define MAX_SESSIONS	12
 
 static struct msgbuf	msgq;
+
+struct rrdp_state {
+	struct pollfd	*pfd;
+	XML_Parser	 parser;
+};
 
 char *
 xstrdup(const char *s)
@@ -42,12 +50,21 @@ xstrdup(const char *s)
 void
 proc_rrdp(int fd)
 {
+	struct rrdp_state state[MAX_SESSIONS];
 	struct pollfd pfds[MAX_SESSIONS + 1];
+	char buf[READ_BUF_SIZE];
+	size_t i;
 
 	if (pledge("stdio recvfd", NULL) == -1)
 		err(1, "pledge");
 
+	memset(&state, 0, sizeof(state));
 	memset(&pfds, 0, sizeof(pfds));
+
+	for (i = 0; i < MAX_SESSIONS; i++) {
+		pfds[1 + i].fd = -1;
+		state[i].pfd = &pfds[1 + i];
+	}
 
 	msgbuf_init(&msgq);
 	msgq.fd = fd;
@@ -55,9 +72,15 @@ proc_rrdp(int fd)
 	pfds[0].fd = fd;
 
 	for (;;) {
-		pfds[0].events |= POLLIN;
+		pfds[0].events = POLLIN;
 		if (msgq.queued)
 			pfds[0].events |= POLLOUT;
+
+		for (i = 0; i < MAX_SESSIONS; i++) {
+			pfds[1 + i].events = 0;
+			if (pfds[1 + i].fd != -1)
+				pfds[1 + i].events = POLLIN;
+		}
 
 		if (poll(pfds, sizeof(pfds) / sizeof(pfds[0]), INFTIM) == -1)
 			err(1, "poll");
@@ -65,15 +88,41 @@ proc_rrdp(int fd)
 		if (pfds[0].revents & POLLHUP)
 			break;
 
-		if (pfds[0].revents & POLLIN) {
-		}
-
 		if (pfds[0].revents & POLLOUT) {
 			switch (msgbuf_write(&msgq)) {
 			case 0:
 				errx(1, "write: connection closed");
 			case -1:
 				err(1, "write");
+			}
+		}
+
+		if (pfds[0].revents & POLLIN) {
+		}
+
+		for (i = 0; i < MAX_SESSIONS; i++) {
+			if (state[i].pfd->revents & POLLHUP) {
+				/* XXX TODO */
+				continue;
+			}
+			if (state[i].pfd->revents & POLLIN) {
+				ssize_t s;
+				XML_Parser p = state[i].parser;
+
+				s = read(state[i].pfd->fd, buf, sizeof(buf));
+				if (s == -1) {
+					/* XXX */
+					warn("read");
+					continue;
+				}
+				if (!XML_Parse(p, buf, s, 0)) {
+					warn("Parse error at line %lu:\n%s\n",
+					    XML_GetCurrentLineNumber(p),
+					    XML_ErrorString(XML_GetErrorCode(p))
+					    );
+					/* XXX */
+					continue;
+				}
 			}
 		}
 	}
